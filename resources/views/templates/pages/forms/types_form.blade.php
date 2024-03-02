2fae0e224b7a58f4bb724ec50e9863c7db93a1eb
arunsai.gandham <arunsai.gandham@karkinos.in>
2024-03-02 18:25:09 +0530

    added color picker

--- resources/views/templates/pages/forms/types_form.blade.php
+++ resources/views/templates/pages/forms/types_form.blade.php
@@ -3,30 +3,24 @@
 @section('title', 'DataTables - Tables')
 
 @section('vendor-style')
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-buttons-bs5/buttons.bootstrap5.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/flatpickr/flatpickr.css') }}" />
-    <!-- Row Group CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-rowgroup-bs5/rowgroup.bootstrap5.css') }}">
-    <!-- Form Validation -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/@form-validation/umd/styles/index.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/jquery-timepicker/jquery-timepicker.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/pickr/pickr-themes.css') }}" />
 @endsection
 
 @section('vendor-script')
-    <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
-    <!-- Flat Picker -->
     <script src="{{ asset('assets/vendor/libs/moment/moment.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/flatpickr/flatpickr.js') }}"></script>
-    <!-- Form Validation -->
-    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/bundle/popular.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/plugin-bootstrap5/index.min.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/@form-validation/umd/plugin-auto-focus/index.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/bootstrap-datepicker/bootstrap-datepicker.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/bootstrap-daterangepicker/bootstrap-daterangepicker.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/jquery-timepicker/jquery-timepicker.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/pickr/pickr.js') }}"></script>
 @endsection
 
 @section('page-script')
-    {{-- <script src="{{ asset('assets/js/tables-datatables-basic.js') }}"></script> --}}
+    <script src="{{ asset('assets/js/forms-pickers.js') }}"></script>
 @endsection
 
 @section('content')
@@ -64,42 +58,33 @@
                                     </div>
                                 </div>
                                 <div class="col-12">
-                               <label for="defaultSelect" class="col-sm-3 col-form-label">Select Color</label>
-                               <select class="form-select mt-2" name="predefined_color" id="predefinedColorSelect">
-                               <option value="">Select a color</option>
-                               <option >Red</option>
-                               <option >Green</option>.9
-                               
-                               <option >Blue</option>
-            <!-- Add more options as needed -->
-                             </select>
-                             </div>
-                            </div>
-
-                            </div>
-                        </div>
-
-                        <div class="col-md-6">
-                            <div>
-                                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-                                <textarea class="form-control" rows="6" name="description" required>{{ isset($type) ? $type->description : '' }}</textarea>
-                            </div>
-                        </div>
-
-                        <div class="pt-4">
-                            <div class="row justify-content-start">
-                                <div class="col-sm-9">
-                                    <input type="hidden" name="id" value="{{ isset($type) ? $type->id : '' }}">
-                                    <button type="submit"
-                                        class="btn btn-primary me-sm-2 me-1 waves-effect waves-light">{{ isset($type) ? 'Update' : 'Submit' }}</button>
-                                    <button class="btn btn-label-secondary waves-effect"><a
-                                            href="{{ route('type.list') }}">Cancel</a></button>
+                                    <label for="defaultSelect" class="col-sm-3 col-form-label">Select Color</label>
+                                    <input class="form-control" name="predefined_color" type="color" value="#666EE8" />
                                 </div>
                             </div>
+
                         </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div>
+                            <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                            <textarea class="form-control" rows="6" name="description" required>{{ isset($type) ? $type->description : '' }}</textarea>
+                        </div>
+                    </div>
+                    <div class="pt-4">
+                        <div class="row justify-content-start">
+                            <div class="col-sm-9">
+                                <input type="hidden" name="id" value="{{ isset($type) ? $type->id : '' }}">
+                                <button type="submit"
+                                    class="btn btn-primary me-sm-2 me-1 waves-effect waves-light">{{ isset($type) ? 'Update' : 'Submit' }}</button>
+                                <button class="btn btn-label-secondary waves-effect"><a
+                                        href="{{ route('type.list') }}">Cancel</a></button>
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
     </div>
-
 @endsection
